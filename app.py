14e7507862d7463b61f71417426a437fce3f7454
cwpeng <padalab@gmail.com>
2021-04-11 07:12:53 +0800

    Base Version

---
+++ app.py
@@ -0,0 +1,20 @@
+from flask import *
+app=Flask(__name__)
+app.config["JSON_AS_ASCII"]=False
+app.config["TEMPLATES_AUTO_RELOAD"]=True
+
+# Pages
+@app.route("/")
+def index():
+	return render_template("index.html")
+@app.route("/attraction/<id>")
+def attraction(id):
+	return render_template("attraction.html")
+@app.route("/booking")
+def booking():
+	return render_template("booking.html")
+@app.route("/thankyou")
+def thankyou():
+	return render_template("thankyou.html")
+
+app.run(port=3000)
