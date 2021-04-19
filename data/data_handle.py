bba0ffac29b948fbd4981102af2787baa5a63e1f
root <root@ip-172-31-32-117.ap-northeast-1.compute.internal>
2021-04-19 06:32:00 +0000

    revise data 318

--- data/data_handle.py
+++ data/data_handle.py
@@ -12,7 +12,7 @@ def handle(db,path):
             if i and i[-3:] in ["jpg","JPG","PNG","png"]:
                 res+="http"+i+";"
         sql_cmd = f"""
-            INSERT INTO attraction.attractions (id,name,category,description,address,transport,mrt,latitude,longitude,images) VALUES ({data2["_id"]},'{data2["stitle"]}', '{data2["CAT2"]}',"{data2['xbody']}",'{data2["address"]}','{data2["info"]}','{data2["MRT"]}',{float(data2["latitude"])},{float(data2["longitude"])},'{res}');
+            INSERT INTO attraction.attractions (id,name,category,description,address,transport,mrt,latitude,longitude,images) VALUES ({data2["_id"]},'{data2["stitle"]}', '{data2["CAT2"]}',"{data2['xbody'].replace('"','')}",'{data2["address"]}','{data2["info"]}','{data2["MRT"]}',{float(data2["latitude"])},{float(data2["longitude"])},'{res}');
         """
         try:
             db.engine.execute(sql_cmd)
