e13555e5d25bf77bf9ed7e742b6f609ef0f7512b
Tony Tsai <tonyny58@gmail.com>
2021-05-31 20:59:29 +0800

    修正booking api錯誤

--- booking.py
+++ booking.py
@@ -9,32 +9,33 @@ def api_book():
 	if "name" not in session:
 		return jsonify({"error":True,"message":"未登入系統"}),403
 	if request.method=="POST":
-		try:
-			import time
-			data = request.get_json()
-			time = data.get("time")
-			price = data.get("price")
-			date = data.get("date")
-			attractionid = data.get("attractionId")
-			if not date or not time or not price:
-				return jsonify({"error":True,"message":"有資料未輸入"}),400
-			now = time.strftime("%Y-%m-%d",time.localtime())
-			gap = datetime.strptime(date,"%Y-%m-%d")-datetime.strptime(now,"%Y-%m-%d")
-			if gap.days<=0:
-				return jsonify({"error":True,"message":"請勿選擇過去時間"}),400
-			if not session.get("google") and not session.get("FB"):
-				idx = session["id"]
-				sql = f"insert into booking (attractionId,date,time,price,userid) values ('{attractionid}','{date}','{time}','{price}','{idx}')"
-			if session.get("google"):
-				email = session["email"]
-				sql = f"insert into booking (attractionId,date,time,price,email) values ('{attractionid}','{date}','{time}','{price}','{email}')"
-			if session.get("FB"):
-				idx = session["FB_ID"]
-				sql = f"insert into booking (attractionId,date,time,price,FB_ID) values ('{attractionid}','{date}','{time}','{price}','{idx}')"
-			db.engine.execute(sql)
-			return jsonify({"ok":True})
-		except:
-			return jsonify({"error":True,"message":"伺服器內部錯誤"}),500
+		# try:
+		import time as tm
+		data = request.get_json()
+		time = data.get("time")
+		price = data.get("price")
+		date = data.get("date")
+		attractionid = data.get("attractionId")
+		if not date or not time or not price:
+			return jsonify({"error":True,"message":"有資料未輸入"}),400
+		now = tm.strftime("%Y-%m-%d",tm.localtime())
+		print(now)
+		gap = datetime.strptime(date,"%Y-%m-%d")-datetime.strptime(now,"%Y-%m-%d")
+		if gap.days<=0:
+			return jsonify({"error":True,"message":"請勿選擇過去時間"}),400
+		if not session.get("google") and not session.get("FB"):
+			idx = session["id"]
+			sql = f"insert into booking (attractionId,date,time,price,userid) values ('{attractionid}','{date}','{time}','{price}','{idx}')"
+		if session.get("google"):
+			email = session["email"]
+			sql = f"insert into booking (attractionId,date,time,price,email) values ('{attractionid}','{date}','{time}','{price}','{email}')"
+		if session.get("FB"):
+			idx = session["FB_ID"]
+			sql = f"insert into booking (attractionId,date,time,price,FB_ID) values ('{attractionid}','{date}','{time}','{price}','{idx}')"
+		db.engine.execute(sql)
+		return jsonify({"ok":True})
+		# except:
+		# 	return jsonify({"error":True,"message":"伺服器內部錯誤"}),500
 	if request.method=="GET":
 		idx = session.get("id")
 		email = session.get("email")
