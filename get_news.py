41ad672ad1a4f71ac9fae03310695a7cd458aad9
Tony Tsai <tonyny58@gmail.com>
2021-06-25 07:30:45 +0800

    more day

--- get_news.py
+++ get_news.py
@@ -6,10 +6,11 @@ from main import db_RDS
 
 
 now = date.today()
-delta = timedelta(days=1)
+now = now+timedelta(hours=8)
+delta = timedelta(days=2)
 
 urls_list = []
-for i in range(1):
+for i in range(2):
     urls_list.append(f"https://www.ettoday.net/news/news-list-{now.strftime('%Y-%m-%d')}-0.htm")
     now = now - delta
 
@@ -36,7 +37,7 @@ def print_urls(url):
 def run(f,urls):
     s = 0
 
-    with ThreadPoolExecutor(max_workers=8) as executor:
+    with ThreadPoolExecutor(max_workers=3) as executor:
         res = executor.map(f,urls)
     
     return res
