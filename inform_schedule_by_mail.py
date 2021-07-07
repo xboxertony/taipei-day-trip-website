687789ffd07c72a017c5fae0a36b74e893b8d0a6
Tony Tsai <tonyny58@gmail.com>
2021-07-07 12:34:02 +0800

    重設定email

--- inform_schedule_by_mail.py
+++ inform_schedule_by_mail.py
@@ -6,7 +6,7 @@ from flask import render_template
 
 with app.app_context():
 
-    sql = "SELECT * FROM attraction.order where datediff(date,current_date()) < 3 and date>current_date();"
+    sql = "SELECT * FROM attraction.order where datediff(date,current_date()) < 4 and date>current_date();"
 
     data = db_RDS.engine.execute(sql)
 
@@ -27,7 +27,7 @@ with app.app_context():
             }]
             
     for email in [em for em in res.keys()]:
-        if email=="tonyny58@gmail.com":
+        if email in ["tonyny58@gmail.com","x25836901@gmail.com"]:
             msg = Message(subject=f"行前通知，三天內行程",sender=mail_username,recipients=[email])
             msg.html = render_template("inform_schedule_mail.html",data=res[email])
             mail.send(msg)
