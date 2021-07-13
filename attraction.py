24fbf17eea3e22b7f73ef16cca70b04cc001c7bf
Tony Tsai <tonyny58@gmail.com>
2021-07-13 14:04:18 +0800

    更改景點RDS

--- attraction.py
+++ attraction.py
@@ -2,10 +2,9 @@ from flask import Blueprint,request
 from flask.json import jsonify
 from data import data_handle
 import json
-from main import db,db_RDS
+from main import db_RDS
 import requests as req
 from config import ytb_key
-import math,decimal
 
 attraction_app = Blueprint("attraction_app",__name__)
 
@@ -22,7 +21,7 @@ def attr():
 		if cat=="all":
 			cat = None
 		if name:
-			ans = data_handle.filter_by_keyword(db,page,name)
+			ans = data_handle.filter_by_keyword(db_RDS,page,name)
 		elif mrt or cat:
 			if mrt and cat:
 				sql_cmd = f"SELECT count(*) FROM attraction.attractions where mrt='{mrt}' and category='{cat}'"
@@ -33,12 +32,12 @@ def attr():
 			else:
 				sql_cmd = f"SELECT count(*) FROM attraction.attractions where category='{cat}'"
 				sql = f"SELECT * FROM attraction.attractions where category='{cat}' limit 12 offset {max(page-1,0)*12}"
-			data = db.engine.execute(sql_cmd)
+			data = db_RDS.engine.execute(sql_cmd)
 			cntt = 0
 			for i in data:
 				cntt = i[0]
 			# sql = f"SELECT * FROM attraction.attractions where mrt='{mrt}' limit 12 offset {max(page-1,0)*12}"
-			data = db.engine.execute(sql)
+			data = db_RDS.engine.execute(sql)
 			if page*12>cntt:
 				page = None
 			ans = []
@@ -52,7 +51,7 @@ def attr():
 						ans.append(res.copy())
 			return json.dumps({"nextPage":page,"data":ans},ensure_ascii=False)
 		else:
-			ans = data_handle.filter_by_page(db,page)
+			ans = data_handle.filter_by_page(db_RDS,page)
 		return ans
 	except Exception as e:
 		print(e)
@@ -61,7 +60,7 @@ def attr():
 @attraction_app.route("/api/attraction/<id>")
 def attr2(id):
 	try:
-		res = data_handle.filter_by_id(db,id)
+		res = data_handle.filter_by_id(db_RDS,id)
 		if not res:
 			return json.dumps({"error":True,"message":"景點編號錯誤"},ensure_ascii=False),400
 		return res
@@ -71,7 +70,7 @@ def attr2(id):
 @attraction_app.route("/api/mrt")
 def mrt():
 	sql = f"SELECT distinct mrt FROM attraction.attractions where mrt!='None'"
-	data = db.engine.execute(sql)
+	data = db_RDS.engine.execute(sql)
 	ans = []
 	for i in data:
 		ans.append(i[0])
@@ -80,7 +79,7 @@ def mrt():
 @attraction_app.route("/api/cat")
 def cat():
 	sql = f"SELECT distinct category FROM attraction.attractions"
-	data = db.engine.execute(sql)
+	data = db_RDS.engine.execute(sql)
 	ans = []
 	for i in data:
 		ans.append(i[0])
